b06103990b42c9f4fdf42639f0125c704c5a316d
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-20 10:11:19 -0500

    basic deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
 // write your code here
+
+const ramenMenu = document.querySelector('#ramen-menu')
+const ramenDetail = document.querySelector('#ramen-detail')
+const ramenRating = document.querySelector('#ramen-rating')
+const ramenDetailImage = document.querySelector('.detail-image')
+const ramenName = document.querySelector('.name')
+const ramenRestaurant = document.querySelector('.restaurant')
+const ramenComment = document.querySelector('#comment')
+const ramenFormRating = document.querySelector('#rating')
+
+
+fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(data => createRamenImgs(data));
+
+
+
+  function createRamenImgs(ramenData) {
+      ramenData.forEach(ramen => {
+          ramenMenu.innerHTML += `
+            <img class='ramen-${ramen.id}' data-id='${ramen.id}' src='${ramen.image}' alt='${ramen.name}'>
+          `
+      });
+  }
+
+
+  ramenMenu.addEventListener('click', function(e) {
+    fetch(`http://localhost:3000/ramens/${e.target.dataset.id}`)
+    .then(response => response.json())
+    .then(data => updateRamenPage(data));
+  })
+
+
+  function updateRamenPage(data) {
+      ramenDetailImage.src = data.image
+      ramenName.innerText = data.name
+      ramenRestaurant.innerText = data.restaurant
+      ramenComment.innerText = data.comment
+      ramenComment.dataset.id = data.id
+      ramenFormRating.value = data.rating
+  }
+
+
+
+  ramenRating.addEventListener('submit', function(e) {
+      e.preventDefault()
+
+      newRating = e.target.rating.value
+      newComment = e.target.comment.value
+      ramenId = e.target.comment.dataset.id
+      const updatedRatingObj = {
+        rating: newRating,
+        comment: newComment,
+        id: ramenId
+      }
+      updateOnBackend(updatedRatingObj)
+      e.target.reset()
+  })
+
+
+  function updateOnBackend(data) {
+      fetch(`http://localhost:3000/ramens/${data.id}`, {
+        method: 'PATCH', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
